fd8229d6c550f2c5a771e15ade173d78f74e2de8
Josh Lawlor <joshlawlor1@gmail.com>
2025-01-03 11:54:02 -0500

    Leap Year Function Problem: Solved

--- Practice Problems Folder/Leap Year Function/leapyear.py
+++ Practice Problems Folder/Leap Year Function/leapyear.py
@@ -5,9 +5,19 @@ def is_leap(year):
     
     # Write your logic here
     #IF year is divisible by 4 AND cannot be divided by 100 = leap year true
-    #ELSE IF year is also evenly divisible by 400 = leap year true
+     #ELSE IF year is also evenly divisible by 400 = leap year true
     #ELSE = leap year false
     
+    
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+    else:
+        leap = False
+   
     return leap
 
 year = random.randint(1900, 25000)
