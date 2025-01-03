0e5faac7d8df5803689350bb641ca3fbff2f5145
Josh Lawlor <joshlawlor1@gmail.com>
2025-01-03 13:34:28 -0500

    Print Function modified to one line solution, simpler

--- Practice Problems Folder/Print Function/printfunction.py
+++ Practice Problems Folder/Print Function/printfunction.py
@@ -1,6 +1,3 @@
 if __name__ == '__main__':
     n = int(4)
-    array = []
-    for i in range(1,n+1):
-        array.append(i)
-print(*array, sep='', end='\n')       
+    print(''.join(map(str, range(1, n + 1))))     
