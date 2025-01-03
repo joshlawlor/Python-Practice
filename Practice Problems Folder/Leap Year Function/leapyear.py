26977c63ddab4c962bc42de516d39bd547ffdac2
Josh Lawlor <joshlawlor1@gmail.com>
2025-01-03 11:36:49 -0500

    Pseudo code written out for leap year function

--- Practice Problems Folder/Leap Year Function/leapyear.py
+++ Practice Problems Folder/Leap Year Function/leapyear.py
@@ -4,6 +4,9 @@ def is_leap(year):
     leap = False
     
     # Write your logic here
+    #IF year is divisible by 4 AND cannot be divided by 100 = leap year true
+    #ELSE IF year is also evenly divisible by 400 = leap year true
+    #ELSE = leap year false
     
     return leap
 
